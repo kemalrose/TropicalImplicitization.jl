e21042292dc5fb3630589066b67aa400d1a54d82
kemalrose <kemalrose@t-online.de>
2024-10-24 08:10:50 +0200

    Added code

--- src/TropicalImplicitization.jl
+++ src/TropicalImplicitization.jl
@@ -6,9 +6,9 @@
 
 module TropicalImplicitization
 
-using Oscar, LinearAlgebra
+using Oscar, LinearAlgebra, Combinatorics
 
-export get_trop_A_disc, get_polytope_from_cycle, compute_A_discriminant, get_dual_fan_of_chow_polytope, get_tropical_cycle, get_vandermonde_matrix, sample, get_chow_fan, get_aux_data, getVertex
+export get_trop_A_disc, get_polytope_from_cycle, compute_A_discriminant, get_dual_fan_of_chow_polytope, get_tropical_cycle, get_vandermonde_matrix, sample, get_chow_fan, get_aux_data, getVertex, extract_polar_degrees, get_trop_con_Var, compute_polar_degrees
 include("functions.jl")
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,4 +5,9 @@ using Test
     A = [1 1 1 1 1 1 1 1; 0 0 0 0 1 1 1 1; 0 0 1 1 0 0 1 1; 0 1 0 1 0 1 0 1]
     cone_list, weight_list = get_trop_A_disc(A)
     Delta = get_polytope_from_cycle(cone_list, weight_list)
+
+    A = [1 1 1 1 1 1; 0 1 0 1 0 1; 0 0 1 1 0 0; 0 0 0 0 1 1]
+    cone_list, weight_list = TropicalImplicitization.get_trop_con_Var(A)
+    TropicalImplicitization.extract_polar_degrees(cone_list, weight_list)
+    TropicalImplicitization.compute_polar_degrees(A)
 end
